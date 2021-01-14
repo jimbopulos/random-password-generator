2e55d046108a4795f2904ea24e6c66e30d35f923
James Galantino <james_galantino@aol.com>
2021-01-13 20:59:20 -0500

    Created arrays for lowercase, uppercase, special, and numeric characters respectively

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// create array for lowercase letters
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//create array for uppercase letters
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L","M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// create array for special characters
+var specChar = [" ", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+// create array for numeric characters
+var numChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+console.log(lowerCase);
+console.log(upperCase);
+console.log(specChar);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -24,6 +40,7 @@ generateBtn.addEventListener("click", writePassword);
 
 // user prompted for character types to include in the password
 // can choose lowercase, uppercase, numeric, and/or special characters
+
 // as each prompt answered, input should be validated and at least one      character type should be selected
 
 // once all prompts answered, password that matches the selected criteria generated
