e0ccc127cc8ff9d469e5ceff65c2a20cf9add289
James Galantino <james_galantino@aol.com>
2021-01-13 20:28:19 -0500

    Wrote out pseudocode for script.js

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// generate password
+// when generate password but clickedd, user presented with a series of prompts for password criteria
+
+// user prompted for password criteria
+// user selects which criteria to include in the password
+
+// user prompted for the length of the password
+// they chooses length of 8 characters min, 128 characters max
+
+// user prompted for character types to include in the password
+// can choose lowercase, uppercase, numeric, and/or special characters
+// as each prompt answered, input should be validated and at least one      character type should be selected
+
+// once all prompts answered, password that matches the selected criteria generated
+// password is either displayed in an alert or written to the page
