a2fdf0a66ae2a88336cb2af8e257f9151062a4be
James Galantino <james_galantino@aol.com>
2021-01-24 23:20:43 -0500

    Debugged and fixed issues with button not generating prompts/confirms, prompt not alerting char length restrictions

--- script.js
+++ script.js
@@ -12,72 +12,60 @@ var numbersOptions = "0123456789";
 
 var allOptions = " ";
 
-// set prompts
-
-var passwordLength = prompt("How many characters would you like? Choose an amount from 8 to 128");
-
-// if (passwordLength < 8 || passwordLength > 128) {
-//     var invalid = alert("Please enter an amount from 8 to 128");
-//     // redirect user to initial prompt
-//     generatePassword();
-// } else if (passwordLength >= 8 || passwordLength <= 128) {
-//     // move to next prompt
-// };
-
-var lowerCase = confirm("Would you like to include lowercase letters?");
-
-var upperCase = confirm("Would you like to include uppercase letters?");
-
-var symbols = confirm("Would you like to include symbols?");
-
-var numbers = confirm("Would you like to include numbers?");
-
-// if statements to concatenate strings for password
-if (lowerCase) {
-    allOptions += lowerCaseOptions
-};
-// console.log(allOptions);
-
-if (upperCase) {
-    allOptions += upperCaseOptions
-};
-// console.log(allOptions);
-
-if (symbols) {
-    allOptions +=  symbolsOptions
-};
-// console.log(allOptions);
-
-if (numbers) {
-    allOptions += numbersOptions
-};
-// console.log(allOptions);
-
-function generatePassword() {
-    // declare var result
-    var randomPassword = "";
-    // will establish designated password length
-for (var i = 0; i < passwordLength; i++) {
-    // random password generated based on chosen Options
-        randomPassword += allOptions.charAt(
-        Math.floor(Math.random() * allOptions.length)
-        );
-    // string assembling using selected Options of characters
-    } return randomPassword;
-};
-
 // Write password to the #password input
 function writePassword() {
-  // get the random password
-  var password = generatePassword();
-  // get the element where password will live on page
-  var passwordText = document.querySelector("#password");
-  
-  // put the password on the page
-  passwordText.value = password;
+    var passwordLength = prompt("How many characters would you like? Choose an amount from 8 to 128");
+    // if statement to disallow password less than 8 char or more than 128
+    if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please select between 8 and 128 characters");
+    } else {    function generatePassword() {
+        var lowerCase = confirm("Would you like to include lowercase letters?");
+        
+        var upperCase = confirm("Would you like to include uppercase letters?");
+        
+        var symbols = confirm("Would you like to include symbols?");
+        
+        var numbers = confirm("Would you like to include numbers?");
+        
+        // if statements to concatenate strings for password
+        if (lowerCase) {
+            allOptions += lowerCaseOptions
+        };
+        // console.log(allOptions);
+        
+        if (upperCase) {
+            allOptions += upperCaseOptions
+        };
+        // console.log(allOptions);
+        
+        if (symbols) {
+            allOptions +=  symbolsOptions
+        };
+        // console.log(allOptions);
+        
+        if (numbers) {
+            allOptions += numbersOptions
+        };
+        // declare var result
+        var randomPassword = "";
+            // will establish designated password length
+            for (var i = 0; i < passwordLength; i++) {
+                // random password generated based on chosen Options
+                randomPassword += allOptions.charAt(
+                Math.floor(Math.random() * allOptions.length)
+                );
+                // string assembling using selected Options of characters
+                } return randomPassword;
+            };
+        // console.log(allOptions); 
+      // get the random password
+      var password = generatePassword();
+      // get the element where password will live on page
+      var passwordText = document.querySelector("#password");
+      // put the password on the page
+      passwordText.value = password;
+    }
 };
-
-writePassword();
 
 // Add event listener to generate button
 // user clicks generate button
