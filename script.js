a682e7155684dbf0f0b1301f281e53a57ad7eb2a
James Galantino <james_galantino@aol.com>
2021-01-24 23:26:37 -0500

    Added appropriate options for newly understood and refactored code

--- script.js
+++ script.js
@@ -12,13 +12,16 @@ var numbersOptions = "0123456789";
 
 var allOptions = " ";
 
-// Write password to the #password input
+// Button to begin prompts/confirms for password
 function writePassword() {
+    // set var for password length
     var passwordLength = prompt("How many characters would you like? Choose an amount from 8 to 128");
     // if statement to disallow password less than 8 char or more than 128
     if (passwordLength < 8 || passwordLength > 128) {
     alert("Please select between 8 and 128 characters");
+    // allow confirm series for char fields if input satisfies char length
     } else {    function generatePassword() {
+        // arrays for possible characters
         var lowerCase = confirm("Would you like to include lowercase letters?");
         
         var upperCase = confirm("Would you like to include uppercase letters?");
@@ -46,24 +49,23 @@ function writePassword() {
         if (numbers) {
             allOptions += numbersOptions
         };
-        // declare var result
+        // declare var for result of all confirms
         var randomPassword = "";
-            // will establish designated password length
+            // loop to go through previously selected options
             for (var i = 0; i < passwordLength; i++) {
                 // random password generated based on chosen Options
                 randomPassword += allOptions.charAt(
                 Math.floor(Math.random() * allOptions.length)
                 );
-                // string assembling using selected Options of characters
+                // string assembling user selected Options of characters
                 } return randomPassword;
-            };
-        // console.log(allOptions); 
-      // get the random password
-      var password = generatePassword();
-      // get the element where password will live on page
-      var passwordText = document.querySelector("#password");
-      // put the password on the page
-      passwordText.value = password;
+            }; // console.log(allOptions); 
+        // get the random password, set variable for called function
+        var password = generatePassword();
+        // get the element where password will live on page
+        var passwordText = document.querySelector("#password");
+        // put the password on the page (value of passwordText var is function called)
+        passwordText.value = password;
     }
 };
 
