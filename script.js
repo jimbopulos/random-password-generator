c9482d42380c3553faef0f01aaa9f18f7073a926
James Galantino <james_galantino@aol.com>
2021-01-28 22:26:19 -0500

    Moved writePassword function to top of page, moved vital generatePassword code within block. Restated prompts/confirms to disallow non-number entry for length

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// write password to input area
+function writePassword() {
+    // get the random password, set variable for called function
+    var password = generatePassword();
+    // get the element where password will live on page
+    var passwordText = document.querySelector("#password");
+    // put the password on the page (value of passwordText var is function called)
+    passwordText.value = password;
+};
+
 // create arrays
 var lowerCaseOptions = "abcdefghijklmnopqrstuvwxyz";
 
@@ -12,24 +22,30 @@ var numbersOptions = "0123456789";
 
 var allOptions = " ";
 
+// declare falseys
+var lowerCase = false;
+var upperCase = false;
+var symbols = false;
+var numbers = false;
+
 // Button to begin prompts/confirms for password
-function writePassword() {
+function generatePassword() {
     // set var for password length
     var passwordLength = prompt("How many characters would you like? Choose an amount from 8 to 128");
     // if statement to disallow password less than 8 char or more than 128
-    if (passwordLength < 8 || passwordLength > 128) {
+    if (passwordLength >= 8 && passwordLength <= 128) {
+        // arrays for possible characters
+        lowerCase = confirm("Would you like to include lowercase letters?");
+        upperCase = confirm("Would you like to include uppercase letters?");
+        symbols = confirm("Would you like to include symbols?");
+        numbers = confirm("Would you like to include numbers?");
+    // if user does not enter a number or blank, disallow
+    } else if (passwordLength !== "" && passwordLength !== null) {
     alert("Please select between 8 and 128 characters");
     // allow confirm series for char fields if input satisfies char length
-    } else {    function generatePassword() {
-        // arrays for possible characters
-        var lowerCase = confirm("Would you like to include lowercase letters?");
-        
-        var upperCase = confirm("Would you like to include uppercase letters?");
-        
-        var symbols = confirm("Would you like to include symbols?");
-        
-        var numbers = confirm("Would you like to include numbers?");
-        
+    } else {
+        alert("Cannot generate password without at least one character type selection. Please try again")
+    }   
         // if statements to concatenate strings for password
         if (lowerCase) {
             allOptions += lowerCaseOptions
@@ -59,15 +75,7 @@ function writePassword() {
                 );
                 // string assembling user selected Options of characters
                 } return randomPassword;
-            }; // console.log(allOptions); 
-        // get the random password, set variable for called function
-        var password = generatePassword();
-        // get the element where password will live on page
-        var passwordText = document.querySelector("#password");
-        // put the password on the page (value of passwordText var is function called)
-        passwordText.value = password;
-    }
-};
+            }; // console.log(allOptions);
 
 // Add event listener to generate button
 // user clicks generate button
